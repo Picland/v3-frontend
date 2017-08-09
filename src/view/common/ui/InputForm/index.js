b169090dc2adb97f233c25fc4c814bb677a07f1c
muwenzi <mrgaonju@gmail.com>
2017-08-09 12:29:59 +0800

    :bug: fix the issue of onClick event and state change

--- src/view/common/ui/InputForm/index.js
+++ src/view/common/ui/InputForm/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import CSSModules from 'react-css-modules'
 import PropTypes from 'prop-types'
+import classNames from 'classnames'
 // import autobind from 'autobind-decorator'
 import '../../style/core.less'
 import styles from './index.less'
@@ -44,44 +45,31 @@ class InputForm extends Component {
     this.setState({
       showEdit: true
     })
-    // debugger
-  }
-
-  _renderEdit () {
-    return (
-      <span styleName="button" onClick={() => this._edit()}>修改</span>
-    )
-  }
-
-  _renderSave () {
-    return (
-      <span>
-        <span styleName="button-cancel" onClick={() => this._cancel()}>取消</span>
-        <span styleName="button" onClick={() => this._save()}>保存</span>
-      </span>
-    )
   }
 
   render () {
     let {label, content, ...others} = this.props
-    let loginHtml
-    if (this.state.showEdit) {
-      loginHtml = this._renderEdit()
-    } else {
-      loginHtml = this._renderSave()
-    }
+    const buttonEdit = classNames({
+      'button': !this.state.showEdit,
+      'button-active': this.state.showEdit
+    })
+    const buttonSave = classNames({
+      'button': this.state.showEdit,
+      'button-active': !this.state.showEdit
+    })
+    const buttonCancel = classNames({
+      'button-cancel': this.state.showEdit,
+      'button-cancel-active': !this.state.showEdit
+    })
     return (
       <div styleName="input-form" {...others}>
         <span styleName="label">{label}</span>
         <span styleName="content">{content}</span>
-        {/* { this.state.showEdit */}
-        {/* ? <span styleName="button" onClick={() => this._edit()}>修改</span> */}
-        {/*: <span> */}
-        {/* <span styleName="button-cancel" onClick={() => this._cancel()}>取消</span> */}
-        {/* <span styleName="button" onClick={() => this._save()}>保存</span> */}
-        {/* </span> */}
-        {/* } */}
-        {loginHtml}
+        <span styleName={buttonEdit} onClick={() => this._edit()}>修改</span>
+        <span>
+          <span styleName={buttonCancel} onClick={() => this._cancel()}>取消</span>
+          <span styleName={buttonSave} onClick={() => this._save()}>保存</span>
+        </span>
       </div>
     )
   }
