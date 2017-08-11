2018731b537f169f7868c30760f8cbc48ff44413
muwenzi <mrgaonju@gmail.com>
2017-08-11 22:40:48 +0800

    :art: add hover and click event

--- src/view/common/ui/InputForm/index.js
+++ src/view/common/ui/InputForm/index.js
@@ -11,7 +11,8 @@ import styles from './index.less'
 class InputForm extends Component {
   static propTypes = {
     label: PropTypes.string,
-    content: PropTypes.string
+    content: PropTypes.string,
+    save: PropTypes.func
   }
 
   static defaultProps = {
@@ -22,33 +23,59 @@ class InputForm extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      showEdit: true
+      showEdit: true,
+      inputProps: {
+        readOnly: 'readOnly'
+      },
+      content: this.props.content
     }
   }
 
   _edit () {
     console.log('edit')
     this.setState({
-      showEdit: false
+      showEdit: false,
+      inputProps: {}
+    })
+    this.contentInput.focus()
+  }
+
+  _handleChange (e) {
+    this.setState({
+      content: e.target.value
     })
   }
 
   _save () {
     console.log('save')
+    this.props.save()
     this.setState({
-      showEdit: true
+      showEdit: true,
+      inputProps: {
+        readOnly: 'readOnly'
+      }
     })
   }
 
   _cancel () {
     console.log('cancel')
     this.setState({
-      showEdit: true
+      showEdit: true,
+      inputProps: {
+        readOnly: 'readOnly'
+      }
+    })
+    this.setState({
+      content: this.props.content
     })
   }
 
   render () {
-    let {label, content, ...others} = this.props
+    let {label} = this.props
+    const inputForm = classNames({
+      'input-form': this.state.showEdit,
+      'input-form-active': !this.state.showEdit
+    })
     const buttonEdit = classNames({
       'button': !this.state.showEdit,
       'button-active': this.state.showEdit
@@ -62,9 +89,14 @@ class InputForm extends Component {
       'button-cancel-active': !this.state.showEdit
     })
     return (
-      <div styleName="input-form" {...others}>
+      <div styleName={inputForm}>
         <span styleName="label">{label}</span>
-        <span styleName="content">{content}</span>
+        <input value={this.state.content}
+               styleName="content"
+               onChange={(e) => this._handleChange(e)}
+               ref={ref => (this.contentInput = ref)}
+               {...this.state.inputProps}
+        />
         <span styleName={buttonEdit} onClick={() => this._edit()}>修改</span>
         <span>
           <span styleName={buttonCancel} onClick={() => this._cancel()}>取消</span>
--- src/view/container/Setting/Profile.js
+++ src/view/container/Setting/Profile.js
@@ -1,30 +1,29 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import CSSModules from 'react-css-modules'
 import styles from './Profile.less'
 import InputForm from '../../common/ui/InputForm'
-// import autobind from 'autobind-decorator'
-// import { connect } from 'react-redux'
-// import { push } from 'react-router-redux'
-// import { loadArticles } from '../../reducer/preview'
+import { connect } from 'react-redux'
 
-// const mapStateToProps = (state) => ({
-//   articleList: state.preview.articleList
-// })
+const mapStateToProps = (state) => ({
+  user: state.user.user
+})
 
-// const mapDispatchToProps = ({
-//   loadArticles,
-//   push
-// })
-
-// connect 方法让组件从redux的状态树中获取数据
-// @connect(mapStateToProps, mapDispatchToProps)
+@connect(mapStateToProps)
 @CSSModules(styles)
 class Profile extends React.Component {
+  static propTypes = {
+    user: PropTypes.object
+  }
+  _save () {
+    console.log('Profile的save')
+  }
   render () {
+    let { user } = this.props
     return (
       <div styleName="container">
         <div styleName="title">个人资料</div>
-        <InputForm label="昵称" content="Kimi Gao" />
+        <InputForm label="昵称" content={user.name} save={() => this._save()} />
       </div>
     )
   }
