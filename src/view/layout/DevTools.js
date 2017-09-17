f6fa62163da1609b887de92070b2aaff23472a5d
muwenzi <mrgaonju@gmail.com>
2017-09-18 00:33:44 +0800

    :star: add redux-devtools-chart-monitor

--- src/view/layout/DevTools.js
+++ src/view/layout/DevTools.js
@@ -1,15 +1,37 @@
+/**
+ * @fileoverview redux devtools config
+ * @reference https://github.com/gaearon/redux-devtools
+ * @reference https://github.com/gaearon/redux-devtools-dock-monitor
+ * @reference https://github.com/romseguy/redux-devtools-chart-monitor
+ */
+
 import React from 'react'
 import { createDevTools } from 'redux-devtools'
-import LogMonitor from 'redux-devtools-log-monitor'
 import DockMonitor from 'redux-devtools-dock-monitor'
-// import ChartMonitor from 'redux-devtools-chart-monitor'
+import LogMonitor from 'redux-devtools-log-monitor'
+import ChartMonitor from 'redux-devtools-chart-monitor'
+
+const tooltipOptions = {
+  disabled: false,
+  indentationSize: 2,
+  style: {
+    'background-color': '#222',
+    opacity: '1',
+    'border-radius': '5px',
+    padding: '5px',
+    color: '#fff'
+  }
+}
 
 const DevTools = createDevTools(
-  <DockMonitor defaultIsVisible={false}
+  <DockMonitor
+    defaultIsVisible={false}
     toggleVisibilityKey="ctrl-h"
     changePositionKey="ctrl-q"
-    changeMonitorKey="ctrl-m">
+    changeMonitorKey="ctrl-m"
+  >
     <LogMonitor theme="tomorrow" />
+    <ChartMonitor invertTheme tooltipOptions={tooltipOptions} />
   </DockMonitor>
 )
 
