568796ef9fc05a5bd10794a7352a198a3b5ff76e
muwenzi <mrgaonju@gmail.com>
2017-09-29 14:45:22 +0800

    :fire: upgrade react from 15 to 16

--- src/view/common/ui/message/index.js
+++ src/view/common/ui/message/index.js
@@ -1,5 +1,6 @@
 import './index.less'
-import React, { PropTypes, Component } from 'react'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import ReactDOM from 'react-dom'
 import classnames from 'classnames'
 import ToggleNode from '../_shared/ToggleNode'
