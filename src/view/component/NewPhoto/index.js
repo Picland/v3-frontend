747d0dc078261b160373e594838ce013b13b9019
muwenzi <mrgaonju@gmail.com>
2017-10-12 01:15:28 +0800

    :art: add basic style of new-photo page

--- src/view/component/NewPhoto/index.js
+++ src/view/component/NewPhoto/index.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import CSSModules from 'react-css-modules'
-// import { NavLink } from 'react-router-dom'
+import { Sticky, StickyContainer } from 'react-sticky'
+import Button from '_common_ui/Button/'
 import Header from '../../layout/Header'
 import Footer from '../../layout/Footer/'
 import styles from './index.less'
@@ -16,13 +17,29 @@ class NewPhoto extends Component {
     let avatarSrc = userInfo ? userInfo.avatar : ''
     return (
       <div styleName="container">
-        <Header buttonLink="/newphoto" buttonName="发布" avatarSrc={avatarSrc} />
-        <div>
-          <div styleName="main">
+        <Header buttonLink="/newphoto" buttonName="发布" avatarSrc={avatarSrc} nofixed />
+        <StickyContainer>
+          <Sticky>
+            {
+              ({ isSticky, wasSticky, style, distanceFromTop, distanceFromBottom, calculatedHeight, styleName }) => {
+                return <div className={styles.navTop} style={style}>
+                  <div className={styles.content}>
+                    <div className={styles.left}>sss</div>
+                    <div className={styles.right}>
+                      <Button type="minor">保存</Button>
+                      <Button>发布</Button>
+                    </div>
+                  </div>
+                </div>
+              }
+            }
+          </Sticky>
+          <div styleName="upload">
             <h2>正在开发中...</h2>
           </div>
-        </div>
-        <Footer />
+          <div styleName="form" />
+          <Footer />
+        </StickyContainer>
       </div>
     )
   }
