64c4d5e25d5757c39f7a06faf7085b140838881b
muwenzi <mrgaonju@gmail.com>
2017-12-02 17:11:52 +0800

    :art: add hover cover on user photo card

--- src/view/app.js
+++ src/view/app.js
@@ -12,46 +12,46 @@ import { initialState } from './redux/reducer/user.reducer'
 import { get } from 'lodash'
 
 async function getInitialState () {
-  let result = await getOwnInfo()
-  if (result.status.code === 0) {
-    return {
-      user: {
-        userInfo: result.data.user,
-        logining: initialState.logining,
-        otherInfo: initialState.otherInfo,
-        registering: initialState.registering
-      }
+    let result = await getOwnInfo()
+    if (result.status.code === 0) {
+        return {
+            user: {
+                userInfo: result.data.user,
+                logining: initialState.logining,
+                otherInfo: initialState.otherInfo,
+                registering: initialState.registering
+            }
+        }
     }
-  }
 }
 
 // 把runtime放到redux状态树上
 (async () => {
-  let initialState = await getInitialState()
-  const store = Store(initialState) // 完整的 Redux 状态树从这里开始生成
-  window.runtime = {
-    userId: get(initialState, 'user.userInfo._id', null)
-  }
-  // const browserHistory = createBrowserHistory() // 将 react-router 中的 browserHistory 移到这里引入
-  // const history = syncHistoryWithStore(browserHistory, store) // 保证 react-router 和 Redux store 的统一
+    let initialState = await getInitialState()
+    const store = Store(initialState) // 完整的 Redux 状态树从这里开始生成
+    window.runtime = {
+        userId: get(initialState, 'user.userInfo._id', null)
+    }
+    // const browserHistory = createBrowserHistory() // 将 react-router 中的 browserHistory 移到这里引入
+    // const history = syncHistoryWithStore(browserHistory, store) // 保证 react-router 和 Redux store 的统一
 
-  // {clientRouter2(history)}
-  const renderApp = () => {
-    render(
-      <Provider store={store}>
-        <div>
-          <App />
-          <div id="loading">正在加载...</div>
-        </div>
-      </Provider>,
-      document.getElementById('root')
-    )
-  }
-  renderApp()
-  // if (__DEVELOPMENT__ && module.hot) {
-  //   console.info('React Moudle/Page Replacement for Dev')
-  //   module.hot.accept('./clientRouter', () => {
-  //     renderApp()
-  //   })
-  // }
+    // {clientRouter2(history)}
+    const renderApp = () => {
+        render(
+            <Provider store={store}>
+                <div>
+                    <App />
+                    <div id="loading">正在加载...</div>
+                </div>
+            </Provider>,
+            document.getElementById('root')
+        )
+    }
+    renderApp()
+    // if (__DEVELOPMENT__ && module.hot) {
+    //   console.info('React Moudle/Page Replacement for Dev')
+    //   module.hot.accept('./clientRouter', () => {
+    //     renderApp()
+    //   })
+    // }
 })()
--- src/view/component/User/User.presentational.js
+++ src/view/component/User/User.presentational.js
@@ -10,63 +10,63 @@ import styles from './index.less'
 
 @CSSModules(styles)
 class User extends Component {
-  static propTypes = {
-    userInfo: PropTypes.object
-  }
-  render () {
-    let { userInfo } = this.props
-    let avatarSrc = userInfo ? userInfo.avatar : ''
-    return (
-      <div styleName="container">
-        { avatarSrc
-          ? <Header buttonLink="/newphoto" buttonName="发布" avatarSrc={avatarSrc} nofixed />
-          : <Header buttonLink="/register" buttonName="注册" nofixed />
-        }
-        <StickyContainer>
-          <div styleName="top-background" style={{backgroundImage: 'url(/img/common/personal_bg.jpg)'}}>
-            <div styleName="homepage-userinfo">
-              <Avatar src={avatarSrc} title="更换头像" />
-              <div styleName="name">{userInfo.name}</div>
-              <div styleName="bio">{userInfo.bio}</div>
-              {/* <div styleName="location">陕西省-西安市-雁塔区</div> */}
-              <div styleName="follow-btn">已关注</div>
+    static propTypes = {
+        userInfo: PropTypes.object
+    }
+    render () {
+        let { userInfo } = this.props
+        let avatarSrc = userInfo ? userInfo.avatar : ''
+        const cards = new Array(16).fill('')
+        return (
+            <div styleName="container">
+                { avatarSrc
+                    ? <Header buttonLink="/newphoto" buttonName="发布" avatarSrc={avatarSrc} nofixed />
+                    : <Header buttonLink="/register" buttonName="注册" nofixed />
+                }
+                <StickyContainer>
+                    <div styleName="top-background" style={{backgroundImage: 'url(/img/common/personal_bg.jpg)'}}>
+                        <div styleName="homepage-userinfo">
+                            <Avatar src={avatarSrc} title="更换头像" />
+                            <div styleName="name">{userInfo.name}</div>
+                            <div styleName="bio">{userInfo.bio}</div>
+                            {/* <div styleName="location">陕西省-西安市-雁塔区</div> */}
+                            <div styleName="follow-btn">已关注</div>
+                        </div>
+                    </div>
+                    <Sticky relative>
+                        {
+                            ({ isSticky, style }) => {
+                                console.log(isSticky)
+                                return <div className={styles.navTop} style={style}>
+                                    <NavLink to="/user" activeClassName={styles.active}>
+                                        <div className={styles.navMenu}>帖子</div>
+                                    </NavLink>
+                                    <NavLink to="/settings/account" activeClassName={styles.active}>
+                                        <div className={styles.navMenu}>收藏夹</div>
+                                    </NavLink>
+                                    <NavLink to="/settings/account" activeClassName={styles.active}>
+                                        <div className={styles.navMenu}>个人资料</div>
+                                    </NavLink>
+                                </div>
+                            }
+                        }
+                    </Sticky>
+                    <div styleName="main">
+                        { cards.map((card, index) =>
+                            <div key={index} styleName="card">
+                                <img src={`img/test/${index + 1}.jpg`} alt="" />
+                                <div styleName="card-info">
+                                    <span>喜欢</span>
+                                    <span>评论</span>
+                                </div>
+                            </div>)
+                        }
+                    </div>
+                    <Footer />
+                </StickyContainer>
             </div>
-          </div>
-          <Sticky relative>
-            {
-              ({ isSticky, style }) => {
-                console.log(isSticky)
-                return <div className={styles.navTop} style={style}>
-                  <NavLink to="/user" activeClassName={styles.active}><div className={styles.navMenu}>帖子</div></NavLink>
-                  <NavLink to="/settings/account" activeClassName={styles.active}><div className={styles.navMenu}>收藏夹</div></NavLink>
-                  <NavLink to="/settings/account" activeClassName={styles.active}><div className={styles.navMenu}>个人资料</div></NavLink>
-                </div>
-              }
-            }
-          </Sticky>
-          <div styleName="main">
-            <div styleName="card"><img src="img/test/1.jpg" alt="" /></div>
-            <div styleName="card"><img src="img/test/2.jpg" alt="" /></div>
-            <div styleName="card"><img src="img/test/3.jpg" alt="" /></div>
-            <div styleName="card"><img src="img/test/4.jpg" alt="" /></div>
-            <div styleName="card"><img src="img/test/5.jpg" alt="" /></div>
-            <div styleName="card"><img src="img/test/6.jpg" alt="" /></div>
-            <div styleName="card"><img src="img/test/7.jpg" alt="" /></div>
-            <div styleName="card"><img src="img/test/8.jpg" alt="" /></div>
-            <div styleName="card"><img src="img/test/9.jpg" alt="" /></div>
-            <div styleName="card"><img src="img/test/10.jpg" alt="" /></div>
-            <div styleName="card"><img src="img/test/11.jpg" alt="" /></div>
-            <div styleName="card"><img src="img/test/12.jpg" alt="" /></div>
-            <div styleName="card"><img src="img/test/13.jpg" alt="" /></div>
-            <div styleName="card"><img src="img/test/14.jpg" alt="" /></div>
-            <div styleName="card"><img src="img/test/15.jpg" alt="" /></div>
-            <div styleName="card"><img src="img/test/16.jpg" alt="" /></div>
-          </div>
-          <Footer />
-        </StickyContainer>
-      </div>
-    )
-  }
+        )
+    }
 }
 
 export default User
